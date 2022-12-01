3950bb5faaeb796269633d041a428529ba0b1087
Jack Anderson <jack@jackanderson.dev>
2022-12-01 09:39:35 -0500

    day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,38 @@
 defmodule AdventOfCode.Day01 do
+  def parse(args) do
+    args
+    |> String.trim("\n")
+    |> String.split("\n\n")
+    |> Enum.map(&String.split(&1, "\n"))
+    |> Enum.map(&sum_cal_array(&1))
+    |> Enum.sort(&(&1 >= &2))
+  end
+
   def part1(args) do
+    args
+    |> parse()
+    |> Enum.at(0)
   end
 
   def part2(args) do
+    args
+    |> parse()
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  def sum_cal_array(cal_array) do
+    cal_array
+    |> Enum.map(&cal_string_to_integer(&1))
+    |> Enum.sum()
+  end
+
+  def cal_string_to_integer(cal_string) do
+    cal_string
+    |> Integer.parse()
+    |> case do
+      {int, _} -> int
+      _ -> nil
+    end
   end
 end
