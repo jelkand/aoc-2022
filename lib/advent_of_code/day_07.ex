2ee3a0617dbfe64faebca8b6ff36ae9cbc38935e
Jack Anderson <jack@jackanderson.dev>
2022-12-07 14:36:01 -0500

    what a mess lol

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,65 @@
 defmodule AdventOfCode.Day07 do
+  def build_tree(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, " "))
+    |> Enum.reduce(%{current_dir: []}, &handle_command/2)
+  end
+
+  def complete_tree(tree = %{current_dir: current_dir}) when current_dir == "/" do
+    tree
+  end
+
+  def complete_tree(tree) do
+    handle_command(["$", "cd", ".."], tree)
+  end
+
   def part1(args) do
+    args
+    |> build_tree()
+    |> complete_tree()
+    |> Map.delete(:current_dir)
+    |> IO.inspect()
+
+    # |> dirs_to_size()
   end
 
   def part2(args) do
+  end
+
+  def handle_command(["$", "cd", ".."], acc) do
+    files_size = Kernel.get_in(acc, acc.current_dir ++ [:files_size])
+    IO.inspect(files_size, label: acc.current_dir)
+
+    acc
+    |> Map.update!(:current_dir, &List.delete_at(&1, -1))
+    |> Kernel.put_in(
+      acc.current_dir ++ [:size],
+      files_size + Kernel.get_in(acc, acc.current_dir ++ [:files_size])
+    )
+  end
+
+  def handle_command(["$", "cd", dir_name], acc),
+    do: Map.update!(acc, :current_dir, &(&1 ++ [dir_name]))
+
+  # doesn't seem to be necessary right now?
+  def handle_command(["$", "ls"], acc),
+    do: Kernel.put_in(acc, acc.current_dir, %{files_size: 0, files: []})
+
+  def handle_command(["dir", dir_name], acc),
+    do: Kernel.put_in(acc, acc.current_dir ++ [dir_name], %{})
+
+  def handle_command([size, name], acc),
+    do: Kernel.update_in(acc, acc.current_dir ++ [:files], &(&1 ++ [{name, get_integer(size)}]))
+
+  # |> Kernel.update_in(acc.current_dir ++ [:files_size], &(&1 + get_integer(size)))
+
+  def get_integer(string) do
+    string
+    |> Integer.parse()
+    |> case do
+      {int, _} -> int
+      _ -> nil
+    end
   end
 end
--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -3,12 +3,66 @@ defmodule AdventOfCode.Day07Test do
 
   import AdventOfCode.Day07
 
-  @tag :skip
+  # test "foo" do
+  #   input = %{"/" => %{files: [{"i", 584}], files_size: 584}}
+
+  #   result = dirs_to_sizes(input)
+
+  #   assert result ==
+  #            %{"/" => %{files: [{"i", 584}], files_size: 584, size: 584}}
+  # end
+
+  # test "foo2" do
+  #   input = %{
+  #     "a" => %{
+  #       :files => [{"f", 29116}, {"g", 2557}, {"h.lst", 62596}],
+  #       :files_size => 94269,
+  #       "e" => %{files: [{"i", 584}], files_size: 584}
+  #     }
+  #   }
+
+  #   result = dirs_to_sizes(input)
+
+  #   assert result == %{
+  #            "a" => %{
+  #              :files => [{"f", 29116}, {"g", 2557}, {"h.lst", 62596}],
+  #              :files_size => 94269,
+  #              "e" => %{files: [{"i", 584}], files_size: 584, size: 584},
+  #              :size => 94853
+  #            }
+  #          }
+  # end
+
   test "part1" do
-    input = nil
+    input = """
+    $ cd /
+    $ ls
+    dir a
+    14848514 b.txt
+    8504156 c.dat
+    dir d
+    $ cd a
+    $ ls
+    dir e
+    29116 f
+    2557 g
+    62596 h.lst
+    $ cd e
+    $ ls
+    584 i
+    $ cd ..
+    $ cd ..
+    $ cd d
+    $ ls
+    4060174 j
+    8033020 d.log
+    5626152 d.ext
+    7214296 k
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 95437
   end
 
   @tag :skip
