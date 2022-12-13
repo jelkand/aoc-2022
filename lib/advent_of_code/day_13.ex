4784a0c844607dc839fb6ad7ffa83be67a1527c5
Jack Anderson <jack@jackanderson.dev>
2022-12-13 11:28:08 -0500

    day 13, but with a bug

--- lib/advent_of_code/day_13.ex
+++ lib/advent_of_code/day_13.ex
@@ -1,5 +1,45 @@
 defmodule AdventOfCode.Day13 do
+  def compare({first, second}) when is_integer(first) and is_integer(second),
+    do: first - second
+
+  def compare({first, second}) when is_integer(first) and is_list(second),
+    do: compare({[first], second})
+
+  def compare({first, second}) when is_list(first) and is_integer(second),
+    do: compare({first, [second]})
+
+  def compare({[], second}) when is_list(second), do: -1
+  def compare({first, []}) when is_list(first), do: 1
+
+  def compare({[first_head | first_rest], [second_head | second_rest]}) do
+    case compare({first_head, second_head}) do
+      diff when diff == 0 ->
+        compare({first_rest, second_rest})
+
+      diff when diff < 0 ->
+        diff
+
+      diff ->
+        diff
+    end
+  end
+
+  def parse_strings(pair_str) do
+    String.split(pair_str, "\n", trim: true)
+    |> Enum.map(fn str -> Code.eval_string(str) |> Kernel.elem(0) end)
+    |> List.to_tuple()
+  end
+
   def part1(args) do
+    String.split(args, "\n\n", trim: true)
+    |> tap(fn list -> IO.inspect(length(list), label: "pairs: ") end)
+    |> Enum.map(&parse_strings/1)
+    |> Enum.map(&compare/1)
+    |> Enum.with_index(1)
+    |> Enum.filter(&(Kernel.elem(&1, 0) < 0))
+    |> IO.inspect(label: "diffs")
+    |> Enum.map(&Kernel.elem(&1, 1))
+    |> Enum.sum()
   end
 
   def part2(args) do
--- test/advent_of_code/day_13_test.exs
+++ test/advent_of_code/day_13_test.exs
@@ -3,12 +3,60 @@ defmodule AdventOfCode.Day13Test do
 
   import AdventOfCode.Day13
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+  [1,1,3,1,1]
+  [1,1,5,1,1]
 
-    assert result
+  [[1],[2,3,4]]
+  [[1],4]
+
+  [9]
+  [[8,7,6]]
+
+  [[4,4],4,4]
+  [[4,4],4,4,4]
+
+  [7,7,7,7]
+  [7,7,7]
+
+  []
+  [3]
+
+  [[[]]]
+  [[]]
+
+  [1,[2,[3,[4,[5,6,7]]]],8,9]
+  [1,[2,[3,[4,[5,6,0]]]],8,9]
+  """
+
+  test "parse list string" do
+    assert parse_strings("[1,1,3,1,1]\n[1,1,5,1,1]") == {[1, 1, 3, 1, 1], [1, 1, 5, 1, 1]}
+  end
+
+  test "parse nested list string" do
+    assert parse_strings("[[1],[2,3,4]]\n[[1],4]") == {[[1], [2, 3, 4]], [[1], 4]}
+  end
+
+  test "lists in order" do
+    assert compare({[1, 1, 3, 1, 1], [1, 1, 5, 1, 1]}) < 0
+  end
+
+  test "nested lists in order" do
+    assert compare({[[1], [2, 3, 4]], [[1], 4]}) < 0
+  end
+
+  test "left runs out first 1" do
+    assert compare({[4, 4], [4, 4, 4]}) < 0
+  end
+
+  test "left runs out first 2" do
+    assert compare({[[4, 4], 4, 4], [[4, 4], 4, 4, 4]}) < 0
+  end
+
+  test "part1" do
+    result = part1(@input)
+
+    assert result == 13
   end
 
   @tag :skip
