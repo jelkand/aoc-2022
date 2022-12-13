dc64c799e2ec0b0ea113d69f5467bc781fe3873d
Jack Anderson <jack@jackanderson.dev>
2022-12-13 11:43:16 -0500

    part 1 workign... so far

--- lib/advent_of_code/day_13.ex
+++ lib/advent_of_code/day_13.ex
@@ -1,20 +1,22 @@
 defmodule AdventOfCode.Day13 do
-  def compare({first, second}) when is_integer(first) and is_integer(second),
+  def compare(first, second) when first == second, do: 0
+
+  def compare(first, second) when is_integer(first) and is_integer(second),
     do: first - second
 
-  def compare({first, second}) when is_integer(first) and is_list(second),
-    do: compare({[first], second})
+  def compare(first, second) when is_integer(first) and is_list(second),
+    do: compare([first], second)
 
-  def compare({first, second}) when is_list(first) and is_integer(second),
-    do: compare({first, [second]})
+  def compare(first, second) when is_list(first) and is_integer(second),
+    do: compare(first, [second])
 
-  def compare({[], second}) when is_list(second), do: -1
-  def compare({first, []}) when is_list(first), do: 1
+  def compare([], second) when is_list(second), do: -1
+  def compare(first, []) when is_list(first), do: 1
 
-  def compare({[first_head | first_rest], [second_head | second_rest]}) do
-    case compare({first_head, second_head}) do
+  def compare([first_head | first_rest], [second_head | second_rest]) do
+    case compare(first_head, second_head) do
       diff when diff == 0 ->
-        compare({first_rest, second_rest})
+        compare(first_rest, second_rest)
 
       diff when diff < 0 ->
         diff
@@ -32,9 +34,8 @@ defmodule AdventOfCode.Day13 do
 
   def part1(args) do
     String.split(args, "\n\n", trim: true)
-    |> tap(fn list -> IO.inspect(length(list), label: "pairs: ") end)
     |> Enum.map(&parse_strings/1)
-    |> Enum.map(&compare/1)
+    |> Enum.map(fn {a, b} -> compare(a, b) end)
     |> Enum.with_index(1)
     |> Enum.filter(&(elem(&1, 0) < 0))
     |> IO.inspect(label: "diffs")
@@ -43,5 +44,9 @@ defmodule AdventOfCode.Day13 do
   end
 
   def part2(args) do
+    String.split(args, "\n\n", trim: true)
+    |> Enum.flat_map(&(parse_strings(&1) |> Tuple.to_list()))
+    |> Enum.sort(fn {a, b} -> compare(a, b) <= 0 end)
+    |> IO.inspect()
   end
 end
--- test/advent_of_code/day_13_test.exs
+++ test/advent_of_code/day_13_test.exs
@@ -29,6 +29,21 @@ defmodule AdventOfCode.Day13Test do
   [1,[2,[3,[4,[5,6,0]]]],8,9]
   """
 
+  test "complicated arrays" do
+    assert compare(
+             [[], [1, [3, [0]]], []],
+             [
+               [],
+               [
+                 [0, [5, 3, 0, 1, 0], [3, 0, 5, 7], 10, [2, 8, 5, 0]],
+                 10,
+                 [2, 4, [1], [5, 6, 7], []],
+                 []
+               ]
+             ]
+           ) > 0
+  end
+
   test "parse list string" do
     assert parse_strings("[1,1,3,1,1]\n[1,1,5,1,1]") == {[1, 1, 3, 1, 1], [1, 1, 5, 1, 1]}
   end
@@ -37,33 +52,36 @@ defmodule AdventOfCode.Day13Test do
     assert parse_strings("[[1],[2,3,4]]\n[[1],4]") == {[[1], [2, 3, 4]], [[1], 4]}
   end
 
+  @tag :skip
   test "lists in order" do
-    assert compare({[1, 1, 3, 1, 1], [1, 1, 5, 1, 1]}) < 0
+    assert compare([1, 1, 3, 1, 1], [1, 1, 5, 1, 1]) < 0
   end
 
+  @tag :skip
   test "nested lists in order" do
-    assert compare({[[1], [2, 3, 4]], [[1], 4]}) < 0
+    assert compare([[1], [2, 3, 4]], [[1], 4]) < 0
   end
 
+  @tag :skip
   test "left runs out first 1" do
-    assert compare({[4, 4], [4, 4, 4]}) < 0
+    assert compare([4, 4], [4, 4, 4]) < 0
   end
 
+  @tag :skip
   test "left runs out first 2" do
-    assert compare({[[4, 4], 4, 4], [[4, 4], 4, 4, 4]}) < 0
+    assert compare([[4, 4], 4, 4], [[4, 4], 4, 4, 4]) < 0
   end
 
+  @tag :skip
   test "part1" do
     result = part1(@input)
 
     assert result == 13
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(@input)
 
-    assert result
+    assert result == 140
   end
 end
