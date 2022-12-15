6b9abf5bf7b9de88799ae6787d2437185b640745
Jack Anderson <jack@jackanderson.dev>
2022-12-15 09:31:56 -0500

    day 15 part 1

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -1,5 +1,42 @@
 defmodule AdventOfCode.Day15 do
-  def part1(args) do
+  alias AdventOfCode.Helpers
+
+  def parse_sensor(str) do
+    [x, y, beacon_x, beacon_y] =
+      Regex.scan(~r"-?\d+", str) |> Enum.map(fn [x] -> Helpers.get_integer(x) end)
+
+    distance = abs(x - beacon_x) + abs(y - beacon_y)
+    {{x, y}, {beacon_x, beacon_y}, distance}
+  end
+
+  def part1(args, row) do
+    sensors =
+      String.split(args, "\n", trim: true)
+      |> Enum.map(&parse_sensor/1)
+
+    beacons =
+      Enum.map(sensors, &elem(&1, 1))
+      |> MapSet.new()
+
+    Enum.reduce(sensors, MapSet.new(), fn {{x, y}, _, distance}, acc ->
+      unless abs(y - row) > distance do
+        each_direction_on_row = div(1 + distance * 2 - 2 * abs(y - row), 2)
+
+        points =
+          Helpers.ranges_to_points(
+            {(x - each_direction_on_row)..(x + each_direction_on_row), row..row}
+          )
+          |> List.flatten()
+          |> MapSet.new()
+
+        MapSet.union(acc, points)
+      else
+        acc
+      end
+    end)
+    |> MapSet.difference(beacons)
+    |> IO.inspect()
+    |> MapSet.size()
   end
 
   def part2(args) do
--- lib/advent_of_code/helpers.ex
+++ lib/advent_of_code/helpers.ex
@@ -10,6 +10,16 @@ defmodule AdventOfCode.Helpers do
     end)
   end
 
+  def points_within_distance(x, y, 0) do
+    MapSet.new([{x, y}])
+  end
+
+  def points_within_distance(x, y, distance) do
+    MapSet.new([{x, y}])
+    |> MapSet.union(points_within_distance(x + 1, y, distance - 1))
+    |> MapSet.union(points_within_distance(x - 1, y, distance - 1))
+  end
+
   def get_integer(string) do
     string
     |> Integer.parse()
@@ -21,4 +31,11 @@ defmodule AdventOfCode.Helpers do
 
   def sign(int) when int >= 0, do: 1
   def sign(int) when int < 0, do: -1
+
+  @doc """
+    Takes in a tuple of two ranges and returns all points belonging to both.
+  """
+  def ranges_to_points({x_range, y_range}) do
+    Enum.map(x_range, fn x_val -> Enum.map(y_range, fn y_val -> {x_val, y_val} end) end)
+  end
 end
